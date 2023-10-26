c77a63cec878458c91fb6dfa9bf3e3a2bcf01574
Developer From Jokela <developerfromjokela@gmail.com>
2023-10-26 22:03:02 +0300

    Fix init

--- custom_components/enstoheat/__init__.py
+++ custom_components/enstoheat/__init__.py
@@ -1,8 +1,9 @@
-"""The LD2410 BLE integration."""
-
+"""The Ensto BLE integration."""
+import asyncio
 import logging
+from datetime import timedelta
 
-from bleak_retry_connector import BleakError, close_stale_connections, get_device
+from bleak_retry_connector import BleakError, close_stale_connections, get_device, BLEAK_EXCEPTIONS
 
 from homeassistant.components import bluetooth
 from homeassistant.components.bluetooth.match import ADDRESS, BluetoothCallbackMatcher
@@ -11,8 +12,9 @@ from homeassistant.const import CONF_ADDRESS, EVENT_HOMEASSISTANT_STOP, Platform
 from homeassistant.core import Event, HomeAssistant, callback
 from homeassistant.exceptions import ConfigEntryNotReady
 
-from .const import DOMAIN
+from .const import DOMAIN, UPDATE_SECONDS, DEVICE_TIMEOUT
 from .models import ThermostatData, EnstoThermostatLE
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
 PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.SENSOR]
 
@@ -32,15 +34,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     await close_stale_connections(ble_device)
 
-    ld2410_ble = EnstoThermostatLE(ble_device)
-
-    coordinator = LD2410BLECoordinator(hass, ld2410_ble)
+    ensto_therm = EnstoThermostatLE(ble_device)
 
     try:
-        await ld2410_ble.initialise()
+        await ensto_therm.initialise()
     except BleakError as exc:
         raise ConfigEntryNotReady(
-            f"Could not initialise LD2410B device with address {address}"
+            f"Could not initialise Ensto device with address {address}"
         ) from exc
 
     @callback
@@ -49,7 +49,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         change: bluetooth.BluetoothChange,
     ) -> None:
         """Update from a ble callback."""
-        ld2410_ble.set_ble_device_and_advertisement_data(
+        ensto_therm.set_ble_device_and_advertisement_data(
             service_info.device, service_info.advertisement
         )
 
@@ -62,8 +62,43 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         )
     )
 
+    async def _async_update():
+        """Update the device state."""
+        try:
+            await ensto_therm.update()
+        except BLEAK_EXCEPTIONS as ex:
+            raise UpdateFailed(str(ex)) from ex
+
+
+    startup_event = asyncio.Event()
+    cancel_first_update = ensto_therm.register_callback(lambda *_: startup_event.set())
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name=ensto_therm.name,
+        update_method=_async_update,
+        update_interval=timedelta(seconds=UPDATE_SECONDS),
+    )
+
+    try:
+        await coordinator.async_config_entry_first_refresh()
+    except ConfigEntryNotReady:
+        cancel_first_update()
+        raise
+
+    try:
+        async with asyncio.timeout(DEVICE_TIMEOUT):
+            await startup_event.wait()
+    except asyncio.TimeoutError as ex:
+        raise ConfigEntryNotReady(
+            "Unable to communicate with the device; "
+            f"Try moving the Bluetooth adapter closer to {ensto_therm.name}"
+        ) from ex
+    finally:
+        cancel_first_update()
+
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = ThermostatData(
-        entry.title, ld2410_ble, coordinator
+        entry.title, ensto_therm, coordinator
     )
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
@@ -71,7 +106,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     async def _async_stop(event: Event) -> None:
         """Close the connection."""
-        await ld2410_ble.stop()
+        await ensto_therm.stop()
 
     entry.async_on_unload(
         hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, _async_stop)
